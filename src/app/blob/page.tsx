f3868e735422ee3742f6a1029e418892c2f6362a
hosybinhkog <hosybinhkog@gmail.com>
2023-04-25 14:55:12 +0700

    NGHICH CHO VUI

--- src/app/blob/page.tsx
+++ src/app/blob/page.tsx
@@ -48,6 +48,7 @@ const Page = () => {
           </p>
         </div>
       </div>
+      {/* @ts-ignore */}
       <View className="absolute top-0 flex h-screen w-full flex-col items-center justify-center">
         <Blob />
         <Common />
